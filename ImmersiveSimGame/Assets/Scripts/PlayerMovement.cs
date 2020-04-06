8c0499a2b3ecabb7527f1c89593efe05cc68429a
dsjohnson <34867686+dsmarsh7@users.noreply.github.com>
2020-04-06 18:07:46 -0400

    RedEyedFigure 4/6/2020
    Added Multijump, Player movement, and first person camera abilities

---
+++ ImmersiveSimGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool canJump;
+    public float jumpHeight = 3f;
+    private int jumpsLeft;
+    public int extraJumps;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start(){
+        jumpsLeft = extraJumps;
+    }
+    void Update()
+    {
+        if(isGrounded){
+            jumpsLeft = extraJumps;
+        }
+        if(jumpsLeft > 0){
+            canJump = true;
+        }
+        else{
+            canJump = false;
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+
+
+        if(Input.GetButtonDown("Jump") && canJump){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpsLeft--;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
